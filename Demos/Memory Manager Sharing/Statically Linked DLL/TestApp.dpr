0d6a14f287ed55611ed960e4e6c7e63fb167a89a
Pierre le Riche <pierre@cimso.com>
2026-02-27 15:09:03 +0200

    Add an important note in the "Statically Linked DLL" demo documentation regarding debug mode and sharing of the memory manager between an application and statically linked libraries: The debug mode DLL also has to be linked statically (if debug mode will be used), otherwise the unpredictable nature of DLL unload order may cause issues during the shutdown memory leak reporting.

--- Demos/Memory Manager Sharing/Statically Linked DLL/TestApp.dpr
+++ Demos/Memory Manager Sharing/Statically Linked DLL/TestApp.dpr
@@ -8,7 +8,14 @@ uses
 
 {Note that TestLibrary.dll is statically linked, so it will be initialized before the main application.  This means the
 main application will actually be sharing the memory manager of the DLL.  (If TestLibrary was loaded dynamically then
-it would be sharing the memory manager of the main application.)}
+it would be sharing the memory manager of the main application.)
+
+Sharing the memory manager with a statically linked library also has implications for debug mode:  Since the unload
+order of dynamicaly loaded DLLs is not always predictable, the FastMM_FullDebugMode.dll library may end up being
+unloaded before TestLibrary.dll if the debug library is loaded dynamically, causing an A/V when TestLibrary.dll runs its
+leak reporting code.  When sharing the memory manager with statically linked libraries it is therefore recommended to
+statically link to FastMM_FullDebugMode.dll as well (if debug mode is required) via the
+FastMM_DebugLibraryStaticDependency define.}
 procedure LeakMemory; external 'TestLibrary';
 
 begin
